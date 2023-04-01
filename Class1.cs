12b54574122a77d413a444c19d354d7b1a2c6a63
Denis Osipov <de.osipov@outlook.com>
2023-04-01 12:33:39 +0200

    code on C# to insert string into *.docx

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Open the existing Word document
+        using (WordprocessingDocument doc = WordprocessingDocument.Open("input.docx", true))
+        {
+            // Get the main document part
+            MainDocumentPart mainPart = doc.MainDocumentPart;
+
+            // Get the document's body element
+            Body body = mainPart.Document.Body;
+
+            // Create a new paragraph with the text to insert
+            Paragraph newParagraph = new Paragraph();
+            Run run = new Run();
+            Text text = new Text("This is the text that will be inserted into the document.");
+            run.Append(text);
+            newParagraph.Append(run);
+
+            // Insert the new paragraph at the beginning of the document
+            body.InsertAt(newParagraph, 0);
+
+            // Save the changes to the document
+            mainPart.Document.Save();
+        }
+    }
+}
