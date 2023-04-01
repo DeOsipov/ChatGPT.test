44113a322dede2a05b063ab4284cc2eba7a7ccde
Denis Osipov <de.osipov@outlook.com>
2023-04-01 12:47:21 +0200

    WinForm exe with one button

---
+++ CreateWinFormExeWithButton.cs
@@ -0,0 +1,50 @@
+﻿//request
+//give me a code on C# to create *.exe with 1 windows form button
+
+//Here's an example C# code that creates a Windows Forms application with a single button that, when clicked, displays a message box:
+
+using System;
+using System.Windows.Forms;
+
+namespace MyApplication
+{
+    public class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+
+    public class MainForm : Form
+    {
+        private Button myButton;
+
+        public MainForm()
+        {
+            myButton = new Button();
+            myButton.Text = "Click me";
+            myButton.Click += new EventHandler(myButton_Click);
+
+            this.Controls.Add(myButton);
+            this.Text = "My Application";
+        }
+
+        private void myButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello, world!");
+        }
+    }
+}
+
+/*To create an executable file from this code, you can follow these steps:
+
+1. Open Visual Studio and create a new project of type "Windows Forms App (.NET Framework)".
+2. Replace the default code in the Program.cs file with the code above.
+3. Build the project by selecting "Build Solution" from the "Build" menu.
+4. Once the build is successful, navigate to the "bin/Debug" folder in your project directory.
+5. You should find a file named "MyApplication.exe" there, which is your executable file.
+*/
